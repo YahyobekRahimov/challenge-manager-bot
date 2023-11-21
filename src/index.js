8c9ad2ca7452cb391bf8a2e2b62185b229287490
YahyobekRahimov <rcoder570@gmail.com>
2023-11-21 16:28:53 +0500

    configured for daily use

--- src/index.js
+++ src/index.js
@@ -4,26 +4,36 @@ require('dotenv').config()
 
 const TOKEN = process.env.TOKEN;
 
-let currentDay = 15;
+let currentDay = 16;
 
 const bot = new telegramBot(TOKEN, {polling: true});
 
 const schedule = require('node-schedule');
 
-const job = schedule.scheduleJob('27 13 * * *', function () {
+const job = schedule.scheduleJob('0 5 * * *', function () {
     const message = `📅Day ${currentDay}`;
-    sendMessage(message);
+    sendMessage(message, -995958458);
     currentDay++;
+});
+const askForDailyReport = schedule.scheduleJob('0 21 * * *', function () {
+    const message = `@YahyobekRahimov @TheIlhomjon \n Have you, guys, done today's push-ups???`;
+    sendMessage(message, -995958458);
+});
+const motivateUser = schedule.scheduleJob('0 7 * * *', function () {
+    const message = `@YahyobekRahimov @TheIlhomjon \n Are you ready to do today's push-ups? If yes, reply to me with "Yes" `;
+    sendMessage(message, -995958458);
 });
 
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, `Received your message: ${msg.text}`);
-    bot.sendMessage(chatId, msg);
+    if (msg.text.toLowerCase() === 'yes') {
+        const chatId = msg.chat.id;
+        bot.sendMessage(chatId, `Great! Keep it up!`);
+    }
 })
 
-function sendMessage(message) {
-    bot.sendMessage(743713648, message);
+function sendMessage(message, chatId) {
+    bot.sendMessage(chatId, message);
 }
+
 
 
